52776c29751f8f6325f4a857da2e72b54a53d432
Saša Bajtl <sasabajtl@gmail.com>
2021-07-18 23:56:25 +0200

    Test project

---
+++ main.js
@@ -0,0 +1,93 @@
+let wasmWorker = new Worker('wasm-worker.js');
+let detectedZone;
+let cameraStopped = false;
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext("2d");
+const camera = new Camera(context, processVideo);
+
+/***
+ *
+ * @param video
+ */
+function processVideo(video){
+    context.drawImage(video, 0, 0);
+    let image = context.getImageData(0, 0, canvas.width, canvas.height);
+
+    if(wasmWorker){
+        wasmWorker.postMessage({'cmd': 'processFrame', 'image': image});
+    }
+
+    if (detectedZone){
+        context.beginPath();
+        context.rect(detectedZone.x, detectedZone.y, detectedZone.width, detectedZone.height);
+        context.lineWidth = 2;
+        context.strokeStyle = 'green';
+        context.stroke();
+    }
+
+    if(cameraStopped){
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+/***
+ *
+ */
+function startCamera(){
+    if(!wasmWorker){
+        wasmWorker = new Worker('wasm-worker.js');
+    }
+    setListeners();
+    camera.startCamera();
+    if(cameraStopped){
+        cameraStopped = false;
+    }
+}
+
+/***
+ *
+ */
+function stopCamera(){
+    if(camera){
+        camera.stopCamera();
+        cameraStopped = true;
+    }
+    if(wasmWorker){
+        wasmWorker.terminate();
+        wasmWorker = undefined;
+    }
+}
+
+/***
+ *
+ * @returns {Promise<void>}
+ */
+async function onRuntimeInitialized() {
+    console.log("Document Extractor Initialized!")
+}
+
+/***
+ *
+ * @param zone
+ */
+function setDetectedZone(zone){
+    console.log("Detected zone", zone)
+    detectedZone = zone
+}
+
+/***
+ *
+ */
+function setListeners(){
+    wasmWorker.addEventListener('message', async (e) => {
+        switch(e.data.cmd) {
+            case 'onRuntimeInitialized':
+                await onRuntimeInitialized();
+                break;
+            case 'setDetectedZone':
+                setDetectedZone(e.data.zone);
+                break;
+        }
+    }, false);
+}
+
---
+++ wasm-worker.js
@@ -0,0 +1,19 @@
+var Module = {
+    onRuntimeInitialized: () => {
+        postMessage({'cmd': 'onRuntimeInitialized'});
+
+        let wasmHelper = new HelperModule.WasmHelper();
+
+        addEventListener('message', onMessage);
+
+        function onMessage(e) {
+            switch(e.data.cmd){
+                case "processFrame":
+                    wasmHelper.ProcessFrame(e.data.image)
+                    break;
+            }
+        }
+    }
+    };
+
+importScripts('DocumentExtractor.js', '../helpers/WasmHelper.js');
