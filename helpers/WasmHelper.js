687c513b793eb62d5cf409e0654525382b613226
Saša Bajtl <sasabajtl@gmail.com>
2021-07-24 01:17:02 +0200

    Changes

--- helpers/WasmHelper.js
+++ helpers/WasmHelper.js
@@ -28,23 +28,11 @@ function Free(heapBytes) {
  */
 function ProcessFrame(image) {
     console.time('ProcessFrame')
-    /* if (!this._frame_bytes) {
-         console.log("1");
-         this._frame_bytes = this.ToHeap(image.data);
-     } else if (this._frame_bytes.length !== image.data.length) {
-         this.Free(this._frame_bytes); // free heap memory
-         this._frame_bytes = this.ToHeap(image.data);
-         console.log("2");
-     } else {
-         console.log("3");
-         this._frame_bytes.set(image.data);
-     }
-*/
     this._frame_bytes = this.ToHeap(image.data);
     let extractor = new Module.Extractor();
 
     let zone = extractor.detectMrzZone(this._frame_bytes.byteOffset, image.width, image.height);
-    this.Free(this._frame_bytes);
+    Free(this._frame_bytes);
     let zoneToSend = {
         x: zone.get_x(),
         y: zone.get_y(),
